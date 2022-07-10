46490d917db9f601ddbcf46702ab5377dad434fb
Meetyouafter <anton_levus@mail.ru>
2022-07-10 17:09:58 +0300

    create .eslintrc and fix Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 install: #для первого клонирования репозитария
-npm ci
+	npm ci
 
 brain-games: #для запуска игры
-node bin/brain-games.js
+	node bin/brain-games.js
 
 publish: #очистка общего хранилища от учебных проектов
-npm publish --dry-run
+	npm publish --dry-run
+
+lint: #запуск линтера
+	npx eslint
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-export const hello = console.log ('brain-games\nWelcome to the Brain Games!');
-
+export const hello = console.log('brain-games\nWelcome to the Brain Games!');
 export const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+console.log(`Hello, ${userName}!`);
