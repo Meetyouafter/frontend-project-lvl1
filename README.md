8f5135c75b0979911e9b99f6f09b3247494dc8fc
Meetyouafter <anton_levus@mail.ru>
2022-07-11 07:44:48 +0300

    change README.md Add link to asciinema.org
    finished brain-even.js
    
    create .eslint.yml
    
    delete .eslintrc

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Meetyouafter/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Meetyouafter/frontend-project-lvl1/actions)
 
 <a href="https://codeclimate.com/github/Meetyouafter/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/6ec66220f3b2c01bb4da/maintainability" /></a>
+
+https://asciinema.org/connect/48b2efeb-98ed-4684-88ed-3b16f62a71a5
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,14 +1,28 @@
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 import { userName } from '../src/cli.js'
 
+for (let i = 0; i < 3; i++) {
 let randomNumber = Math.floor(Math.random() * 100);
-
-//первый круг
 let answer = readlineSync.question(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber}\n`)
 if (answer === 'yes') {
-    randomNumber % 2 === 0 ? console.log('Your answer: ' + answer + '\nCorrect!') : console.log(`You answer: yes.\n'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`)
+    if (randomNumber % 2 === 0) {
+        console.log('Your answer: ' + answer + '\nCorrect!')
+    } else {
+        console.log(`You answer: yes.\n'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`)
+        break
+    }
 } else if (answer === 'no') {
-    randomNumber % 2 !== 0 ? console.log('Your answer: ' + answer + '\nCorrect!') : console.log(`You answer: no.\n'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`)
+    if (randomNumber % 2 !== 0) {
+        console.log('Your answer: ' + answer + '\nCorrect!')
+     } else {
+        console.log(`You answer: no.\n'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`)
+        break
+     }
 } else {
     console.log(`You answer: ${answer}.\n${answer} is wrong answer ;(.\nLet's try again, ${userName}!`)
+    break
+}
+console.log(`Congratulations, ${userName}!`)
 }
